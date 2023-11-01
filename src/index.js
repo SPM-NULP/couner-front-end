e1bc8e039f3b258a100412c75466122e306b18db
Roman <roman.baytala@gmail.com>
2023-11-01 23:04:52 +0200

    added header, styles, icons
    Headre needs new styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './normalize.css';
+// import './css/boostrap.css'
+import './css/main.css'
+import './css/animate.css'
+// import './css/boostnav.css'
+// import './css/boostrap0.css'
+import './css/plugins.css'
+import './css/responsive.css'
+import './css/slick-theme.css'
+import './css/slick.css'
+import './css/style.css'
+import './css/styles-login-page.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
